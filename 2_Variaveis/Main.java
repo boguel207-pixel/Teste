e3669dc650faa7d68c65b7f42396ac8f3f19988f
Miguel <boguel207@gmail.com>
2026-08-19 21:32:19 -0300

    A

---
+++ 2_Variaveis/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Dona";
+        String sobrenome = "Goti";
+        int idade = 40;
+        double altura = 1.80;
+        Boolean empregado = false;
+        String telefone = "4002-8922";
+        String endereco = "Jardim Belterra 291";
+        String cidade = "Sao Paulo";
+        String uf = "SP";
+        double salario = 50.00;
+        double valor_hora_extra = 1.0;
+        System.out.println("Nome: " + nome + " " + sobrenome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: : " + altura);
+        System.out.println("Empregado: " + empregado);
+        System.out.println("Telefone: " + telefone);
+        System.out.println("Endereco: " + endereco);
+        System.out.println("Cidade: " + cidade);
+        System.out.println("UF: " + uf);
+        System.out.println("Salario: " + salario);
+        System.out.println("Hora extra: " + valor_hora_extra);
+    }
+}
