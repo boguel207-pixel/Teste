4a2df26d3fba08f033f95339910b02073dc66718
Miguel <boguel207@gmail.com>
2026-08-19 22:21:07 -0300

    revisão

---
+++ 4_strings/Main_4.java
@@ -0,0 +1,8 @@
+public class Main_4 {
+    public static void main(String[] args) {
+        String saudacao = "Hello World";
+        System.out.println(saudacao.length());
+        System.out.println(saudacao.toUpperCase());
+        System.out.println(saudacao.toLowerCase());
+    }
+}
---
+++ Main_3.java
@@ -0,0 +1,17 @@
+public class Main_3 {
+    public static void main(String[] args) {
+        // int valor_1 = 10;
+        // int valor_2 = 20;
+        // int soma = 0;
+
+        // soma = valor_1 + valor_2;
+
+        // System.out.println("O valor 1 eh: " + valor_1);
+        // System.out.println("O valor 2 eh: " + valor_2);
+        // System.out.println("O soma eh: " + soma);
+        
+        int contador = 0;
+        System.out.println(contador++);
+        System.out.println(contador--);
+    }
+}
